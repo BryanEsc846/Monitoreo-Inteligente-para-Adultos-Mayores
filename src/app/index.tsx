c5062e15506046fd301dde7c6aacb6effafb560d
Iliana Granados <ilianagranados.05@gmail.com>
2026-08-30 22:25:05 -0600

    Add: Vitalia logo splash screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
 import { ColoresTema } from '../constants/ColoresTema';
@@ -20,8 +20,7 @@ export default function SplashScreen() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Text style={styles.title}>VitalGuard</Text>
-        <Text style={styles.subtitle}>Cuidando a los que amas</Text>
+        <Image source={require('../../assets/images/LOGO_VITALIA.png')} style={styles.LOGO_VITALIA} resizeMode="contain"/>
         <ActivityIndicator size="large" color={ColoresTema.buttonSecondary} style={styles.loader} />
       </View>
     </SafeAreaView>
@@ -31,7 +30,7 @@ export default function SplashScreen() {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: ColoresTema.background,
+    backgroundColor: 'white',
   },
   container: {
     flex: 1,
@@ -49,6 +48,13 @@ const styles = StyleSheet.create({
     color: ColoresTema.textTitle,
     marginBottom: 30,
   },
+
+  LOGO_VITALIA: { 
+    width: 350, 
+    height: 350, 
+    marginBottom: 20,
+  },
+
   loader: {
     marginTop: 20,
   }
